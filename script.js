ea14223e89f3c9e89f49a072e4e37ed3c0dbf9d3
Joshua D'Silva <35666297+jdsilva6@users.noreply.github.com>
2019-05-17 10:44:30 -0400

    Update script.js

--- script.js
+++ script.js
@@ -1,3 +1,6 @@
 function toggleDarkLight(){$('#darkModeSwitch').on('click', () => {
-    $('.body').addClass('dark');
+    $('.html').addClass('dark');
+    $(this).text(function(i, text){
+        return text === "Dark Mode" ? "Light Mode" : "Dark Mode"
+    })
 })}

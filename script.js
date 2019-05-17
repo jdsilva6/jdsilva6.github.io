0596f325947894d9b30c2858066a82755504f81a
Joshua D'Silva <35666297+jdsilva6@users.noreply.github.com>
2019-05-17 11:06:33 -0400

    Updated Theme Changing Script

--- script.js
+++ script.js
@@ -1,6 +1,7 @@
 function toggleDarkLight(){$('#darkModeSwitch').on('click', () => {
-    $('.html').addClass('dark');
+   
     $(this).text(function(i, text){
         return text === "Dark Mode" ? "Light Mode" : "Dark Mode"
     })
+    $('.body').addClass('dark');
 })}

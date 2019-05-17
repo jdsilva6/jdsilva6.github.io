9f557807a2e97823cb1055cd5a1b1b5e80a9fc0e
Joshua D'Silva <35666297+jdsilva6@users.noreply.github.com>
2019-05-17 11:15:45 -0400

    Applying dark mode to div

--- script.js
+++ script.js
@@ -1,7 +1,3 @@
 function toggleDarkLight(){$('#darkModeSwitch').on('click', () => {
-   
-    $(this).text(function(i, text){
-        return text === "Dark Mode" ? "Light Mode" : "Dark Mode"
-    })
-    $('.body').addClass('dark');
+    $('.container').addClass('dark');
 })}

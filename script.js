775a85e54e29e95d474496478f17f9d8cd71893b
Joshua D'Silva <35666297+jdsilva6@users.noreply.github.com>
2019-05-18 09:52:31 -0400

    Fixed Dar Mode and Updated Contents

--- script.js
+++ script.js
@@ -2,4 +2,6 @@ function toggleDarkLight(){$('#darkModeSwitch').on('click', () => {
     $('#theme').addClass('dark');
     $('h2').addClass('darkmode');
     $('#body').addClass('dark-mode')
+    $(".stylized").addClass('divdark')
+    $('#jumbo').addClass('darkjumbo')
 })}

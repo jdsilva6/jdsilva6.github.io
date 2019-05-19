10829d645d888c5f2c2638b116cd82d83fe333cf
Joshua D'Silva <jdsilva6@uwo.ca>
2019-05-19 00:12:22 -0400

    Update script.js

--- script.js
+++ script.js
@@ -2,6 +2,6 @@ function toggleDarkLight(){$('#darkModeSwitch').on('click', () => {
     $('#theme').addClass('dark');
     $('h2').addClass('darkmode');
     $('#body').addClass('dark-mode')
-    $(".stylized").addClass('divdark')
+    $("div.stylized").addClass('divdark')
     $('#jumbo').addClass('darkjumbo')
 })}

2d512ae4ac4bd7293f9066a0d861ec428e627339
Joshua D'Silva <jdsilva6@uwo.ca>
2019-05-19 20:42:58 -0400

    Reversible Dark Mode

--- script.js
+++ script.js
@@ -1,4 +1,5 @@
 function toggleDarkLight(){$('#darkModeSwitch').on('click', () => {
+    $('#darkModeSwitch').html("Light")
     $('#theme').addClass('dark');
     $('h2').addClass('darkmode');
     $('#body').addClass('dark-mode')
@@ -9,4 +10,20 @@ function toggleDarkLight(){$('#darkModeSwitch').on('click', () => {
     $('#jumbo').addClass('darkjumbo')
     $('.btn').addClass('darkbtn')
     $("#myPic").attr("src", "assets/images/Engineer_Stacked_Reversed.png")
+    $("#darkModeSwitch").attr("onclick", "toggleLightMode()")
+})}
+
+function toggleLightMode(){$('#darkModeSwitch').on('click',() =>{
+    $('#darkModeSwitch').html("Dark")
+    $('#theme').removeClass('dark');
+    $('h2').removeClass('darkmode');
+    $('#body').removeClass('dark-mode')
+    $(".stylizedDiv").removeClass('divdark')
+    $(".stylized").removeClass('darkb')
+    $('.card-body').removeClass('darkCard')
+    $('.card-header').removeClass('darkCardHeader')
+    $('#jumbo').removeClass('darkjumbo')
+    $('.btn').removeClass('darkbtn')
+    $("#myPic").attr("src", "assets/images/Engineer_Stacked_PurpleGrey.png")
+    $("#darkModeSwitch").attr("onclick", "toggleDarkLight()")
 })}
